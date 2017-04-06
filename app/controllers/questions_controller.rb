f611e6c2e2b4dfb4b4d6ed86d97c7cfb93b68289
mazovladimir@gmail.com <root@center.ruzik.net>
2017-04-06 16:57:29 +0300

    Added questions GUI

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,38 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [ :edit, :update, :destroy ]
+
+  def index
+    @questions = Question.all
+  end
 
   def new
-    @question = Question.first
-    @answers = @question.answers
+    @question = Question.new
+    #@question = User.first.questions.first
+    #@answers = @question.answers
+  end
+
+  def edit
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
   end
 
   def update
-    @question = Question.find(params[:id])
     #@question.update(score: 'true') if score_answer == @question.answer
     @question.update(question_params) 
     @question.update(score: true) if @question.correct_answer == @question.examinee_answer
     #redirect_to action: "new"
+  end
+
+  def destroy
+    @question.destroy
   end
 
   private
@@ -20,7 +42,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:examinee_answer)
+    params.require(:question).permit(:examinee_answer, :ask)
   end
 
   def set_user
