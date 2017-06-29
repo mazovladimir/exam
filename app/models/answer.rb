d9afad893cec73a1e23983c69a3cf39a4eb48dfd
mazovladimir@gmail.com <root@center.ruzik.net>
2017-06-29 10:31:45 +0300

    new model question 4

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,9 @@
 class Answer < ApplicationRecord
-  validates :give, length: { minimum: 1, maximum: 1000}
+  validate :validate_answer
 
   belongs_to :question
 
-  #def validate_answer
-  #  self.question.errors.add(:base, "Каждый ответ не должен превышать 1000 символов") if self.give.length > 100
-  #end 
+  def validate_answer
+    self.question.errors.add(:base, "Пожалуйста, введите ответ не меньше 1 и не более 1000 символов") if self.give.length > 1000 || self.give.length < 1
+  end 
 end
--- app/models/correct_answer.rb
+++ app/models/correct_answer.rb
@@ -4,6 +4,6 @@ class CorrectAnswer < ApplicationRecord
   belongs_to :question
 
   def validate_correct_answer
-    self.question.errors.add(:base, "Каждый правильный ответ не должен превышать 1000 символов") if self.give.length > 100
+    self.question.errors.add(:base, "Пожалуйста, введите правильный ответ не меньше 1 и не более 1000 символов") if self.give.length > 1000 || self.give.length < 1
   end
 end
